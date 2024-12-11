02e2babe2e86328cf56127be15848808ad04ee71
Your Name <you@example.com>
2024-12-11 17:56:28 +0300

    new function

--- src/masks.py
+++ src/masks.py
@@ -1,17 +1,18 @@
 from typing import Union
 
 
-def get_mask_card_number(card_number: Union[int, str]) -> str:
+def get_mask_card_number(card_number: Union[int, str]) -> Union[str]:
     """Функция маскировки номера банковской карты"""
 
     unmask_number = str(card_number)
+    masked_number = card_number[0:-12] + " " + "** ****" + card_number[-4:]
 
-    return f"{unmask_number[:4]} {unmask_number[4:6]}** **** {unmask_number[-4:]}"
+    return masked_number
 
 
-def get_mask_account(account: Union[int, str]) -> str:
+def get_mask_account(account: Union[int, str]) -> Union[str]:
     """Функцию маскировки номера банковского счета"""
 
     unmask_account = str(account)
-
-    return f"**{unmask_account[-4:]}"
+    masked_account = account[:4] + " " + "**" + account[-4:]
+    return masked_account
---
+++ src/widget.py
@@ -0,0 +1,20 @@
+from typing import Union
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def mask_account_card(card_number: Union[str]) -> str:
+    """Функция которая умеет обрабатывать информацию как о картах, так и о счетах"""
+
+    if "Visa" or "MasterCard" or "Maestro" in card_number:
+        return get_mask_card_number(card_number)
+
+    elif "Счёт" or "Счет" in card_number:
+        return get_mask_account(card_number)
+
+
+def get_date(data_full: Union[str]) -> str:
+    """Функция для преобразования даты"""
+
+    date = data_full[8:10] + "." + data_full[5:7] + "." + data_full[:4]
+
+    return date
