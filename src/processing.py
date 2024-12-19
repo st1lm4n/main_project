01657deb2c64285175a114f8b576e11a9b3740f2
Your Name <you@example.com>
2024-12-19 17:51:44 +0300

    black check

--- src/processing.py
+++ src/processing.py
@@ -3,7 +3,7 @@ from typing import Union
 
 def filter_by_state(list_dict: list[dict], state: str = "CANCELED") -> list[dict]:
     """Функция возвращает новый список словарей, содержащий только те словари, у которых ключ state
- соответствует указанному значению"""
+    соответствует указанному значению"""
 
     new_list_dict = []
 
@@ -13,7 +13,9 @@ def filter_by_state(list_dict: list[dict], state: str = "CANCELED") -> list[dict
     return new_list_dict
 
 
-def filter_by_date(list_dict: list[dict[str, Union[str, int]]], revers_: bool=True) -> list[dict[str, Union[str, int]]]:
+def filter_by_date(
+    list_dict: list[dict[str, Union[str, int]]], revers_: bool = True
+) -> list[dict[str, Union[str, int]]]:
     """Функция сортировки по дате"""
 
     sorted_list = sorted(list_dict, key=lambda x: x["date"], reverse=revers_)
